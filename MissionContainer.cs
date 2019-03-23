56e6b47435f4d4dcda6250c8d126ff48aad53837
Shira Negbi <shira.negbi@live.biu.ac.il>
2019-03-23 17:40:40 +0200

    Copy the relevant files from the instructions and add comments planning what to do

---
+++ MissionContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+  
+    public class FunctionsContainer
+    {
+       //a dictionary from string to function double func(double d) with delegate, syntax from google
+       // a pointer to a func
+       // indexer - syntax from google
+       // func printavailable
+    }
+}
