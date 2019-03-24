ed8c8808a1fe36c2eaf037ad6a228d262de6f22a
Shira Negbi <shira.negbi@live.biu.ac.il>
2019-03-24 12:40:11 +0200

    Implement the ComposedMission class

--- ComposedMission.cs
+++ ComposedMission.cs
@@ -10,5 +10,29 @@ namespace Excercise_1
     {
         // implement constructors and funcs
         // implement interface finctions
+        private List<DoubleToDouble> Funcs;
+
+        public double Calculate(double value)
+        {
+            double result = value;
+            foreach (DoubleToDouble f in this.Funcs)
+            {
+                result = f(result);
+            }
+            return result;
+        }
+
+        public ComposedMission(String name)
+        {
+            this.Type = "Composed";
+            this.Name = name;
+            this.Funcs = new List<DoubleToDouble>();
+        }
+
+        public ComposedMission Add(DoubleToDouble func)
+        {
+            this.Funcs.Add(func);
+            return this;
+        }
     }
 }
